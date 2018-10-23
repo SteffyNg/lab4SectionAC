6f5f8799d7b3cf85672bd8c4fc8be451a3c67fe2
SteffanyNg <sxng@uw.edu>
2018-10-22 18:32:12 -0700

    finished exercise

--- exericse.R
+++ exericse.R
@@ -2,54 +2,61 @@
 # install.packages("dplyr")
 library(dplyr)
 
-# Read in the NBA team data of the 2016-2017 season from the data directory  
+# Read in the NBA team data of the 2016-2017 season from the data directory
 # into a variable called `team.data` using `read.csv`
 team.data <- read.csv("data/teams.csv", stringsAsFactors = FALSE)
 
 
-# The data.frame team.data should now be accessible to you. 
-# View it, and get some basic information about the number of rows/columns. 
-# Note the "X" preceding some of the column titles as well as the "*" 
+# The data.frame team.data should now be accessible to you.
+# View it, and get some basic information about the number of rows/columns.
+# Note the "X" preceding some of the column titles as well as the "*"
 # following the names of teams that made it to the playoffs that year.
 View(team.data)
 
 
 # Add a column that gives the turnovers to steals ratio (TOV / STL) for each team
-
+team.data <- mutate(team.data, turnovers_to_steal = TOV / STL)
 
 # Sort the teams from lowest turnover/steal ratio to highest
+team.data <- arrange(team.data, turnovers_to_steal)
 
+# Find the average BLK and STL for teams having a TOV greater than the average TOV of all teams
+team.data %>% filter(TOV > mean(TOV)) %>% summarise(avg_BLK = mean(BLK), avg_STL = mean(STL))
 
-#Find the average BLK and STL for teams having a TOV greater than the average TOV of all teams
-
-
-# Get the team that had the highest Total Rebounds (TRB) only with the columns 
+# Get the team that had the highest Total Rebounds (TRB) only with the columns
 # Team and TRB  *using one line of code*
-
+highest_total_rebounds <- team.data %>% filter(TRB == max(TRB)) %>% select(Team, TRB)
 
 # Print only the name of the team that had the highest total rebounds
 # (that also happens to be the greatest team of all time)
-
+print(highest_total_rebounds[, "Team"])
 
 ## Let's change gears!
 
-# Read in the Pokemon data from the data directory  
+# Read in the Pokemon data from the data directory
 # into a variable called `pokemon` using `read.csv`. Remember to not read strings in as factors.
+pokemon <- read.csv("data/Pokemon.csv")
 
-# First, View() the data set to see what info you have to work with 
-
+# First, View() the data set to see what info you have to work with
+View(pokemon)
 
 # Find all the Pokemon that are "Water" or "Ghost" Type 1 and have a speed higher than 50
+water_ghost_types <- pokemon %>% filter(
+  (pokemon$Type.1 == "Water" | pokemon$Type.1 == "Ghost") & pokemon$Speed > "50"
+)
 
 # Find the average HP, median HP, min HP and max HP for each of the type of generations
+hp_stats_per_gen <- pokemon %>% group_by(Generation) %>% summarise(mean(HP), median(HP), min(HP), max(HP))
 
-#FIND THE NUMBER OF POKEMONS THAT ARE LEGENDARY AND THE NUMBER OF POKEMONS THAT ARE NOT LEGENDARY 
-#THAT HAVE A HIGHER ATTACK THAN DEFENSE VALUE
-#NOTE: IT SHOULD BE DONE IN ONE RUN OF DPLYR
-#HINT : Look into count() function by dplyr
+# FIND THE NUMBER OF POKEMONS THAT ARE LEGENDARY AND THE NUMBER OF POKEMONS THAT ARE NOT LEGENDARY
+# THAT HAVE A HIGHER ATTACK THAN DEFENSE VALUE
+# NOTE: IT SHOULD BE DONE IN ONE RUN OF DPLYR
+# HINT : Look into count() function by dplyr
+pokemon %>% filter(Attack > Defense) %>% group_by(Legendary) %>% count()
 
-#Find the generation that has the most number of pokemons with Type.1 as "Fire"
+# Find the generation that has the most number of pokemons with Type.1 as "Fire"
+pokemon %>% filter(Type.1 == "Fire") %>% group_by(Generation) %>% count() %>% arrange(desc(n)) %>% head(1) 
 
 # WHICH Type 1 pokemon has the overall within group max value of SUM of HP, Attack, Defense, Sp..Att, Sp.Def, Speed
-#HINT : LOOK AT THE DATASET! - THERE IS A SUPER EASY WAY TO DO THIS.
-
+# HINT : LOOK AT THE DATASET! - THERE IS A SUPER EASY WAY TO DO THIS.
+pokemon %>% filter(Total == max(Total)) %>% head(1)
